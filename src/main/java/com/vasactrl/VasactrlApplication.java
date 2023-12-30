5ee38e337ef0b9ea49697e718b9c4fb5db4cc9f0
Masaramat <nimatmangs@gmail.com>
2023-12-30 16:05:04 +0100

    Initial commit

---
+++ src/main/java/com/vasactrl/VasactrlApplication.java
@@ -0,0 +1,13 @@
+package com.vasactrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VasactrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VasactrlApplication.class, args);
+	}
+
+}
